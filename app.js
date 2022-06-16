13ff3eccd389d21dabc4f667ddebc6c7d2c334a6
Amkam <amaduskamara@gmail.com>
2022-06-16 20:34:07 +0000

    Add middleware for loging req url and method

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
+// Initialize express app
 const app = express();
+
+// Middlewares
+app.use((req, res, next) => {
+  console.log(req.url, req.method, "Middlewre ran");
+  next();
+});
 
 const PORT = 4000;
 
