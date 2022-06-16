88b4c5fc7386f2270aa05d28fbfd80fc8bd58c7e
Amkam <amaduskamara@gmail.com>
2022-06-16 21:01:15 +0000

    Add routes for GET and POST

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require("express");
 
 // Initialize express app
 const app = express();
+app.use(express.json());
 
 // Middlewares
 app.use((req, res, next) => {
   console.log(req.url, req.method, "Middlewre ran");
   next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ method: "GET method ran" });
+});
+
+app.post("/add-product", (req, res) => {
+  res.json({ method: "POST method added" });
 });
 
 const PORT = 4000;
