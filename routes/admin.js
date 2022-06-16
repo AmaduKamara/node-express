afa696c66609fe87e33d4a60ad0d7dc501ca8db8
Amkam <amaduskamara@gmail.com>
2022-06-16 22:04:39 +0000

    Add util file for root path construction in a cleaner way

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+
+const rootDir = require("../util/path");
 
 // Express router
-const router = express.Router()
+const router = express.Router();
 
 router.get("/add-product", (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
-module.exports = router
+module.exports = router;
